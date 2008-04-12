39a71b06cd26dfa7daf58dca9937c2ab0a3f5619
gustavomartinez <gustavomartinez@8946db71-1644-0410-beab-d1ee511207b3>
2008-04-12 20:06:27 +0000

    AuthSub es ahora extension del archivo con mismo nombre de GData. Se agrega ContactsQuery, permite parametrizar consultas a la api.

--- library/Zsurforce/ZGdata/Gmail.php
+++ library/Zsurforce/ZGdata/Gmail.php
@@ -4,32 +4,38 @@
   
   class ZsurForce_ZGdata_Gmail extends Zend_Gdata{
       
+      const CONTACTS_URI = 'http://www.google.com/m8/feeds/contacts';
+      
       public function __construct($client = null, $applicationId = 'ZsurForce-ZGdata-1.0')
       {
-        //$this->registerPackage('Zend_ZGdata_Gmail');
-        //$this->registerPackage('Zend_ZGdata_Gmail_Extension');
+        $this->registerPackage('Zsurforce_ZGdata_Gmail');
+        //$this->registerPackage('Zsurforce_ZGdata_Gmail_Extension');
         parent::__construct($client, $applicationId);
       }
       
-      public function getContacts($user = null)
-      {
+      public function getContactsEmail($user = null, $location = null)
+    {
         if ($user !== null) {
-            $uri = 'http://www.google.com/m8/feeds/contacts/' . $user . '/base?max-results=300';
+            $uri = self::CONTACTS_URI . "/" . $user . "/base";
+        } else if ($location instanceof Zend_Gdata_Query) {
+            $uri = $location->getQueryUrl();
         } else {
-            require_once 'ZsurForce/ZGdata/Gmail/EmptyArgumentException.php';
-            throw new ZsurForce_ZGdata_Gmail_EmptyArgumentException(
-                    'Falta el p&aacute;rametro "$user"');
+            include_once('Zsurforce/ZGdata/Gmail/EmptyArgumentException.php');
+            throw new ZsurForce_ZGdata_Gmail_EmptyArgumentException('
+                    Los argumenos no existen o no son v&aacute;lidos');
         }
         
         $feed = parent::getFeed($uri, 'Zend_Gdata_Feed');
-      
-        foreach($feed as $entry) {
+        $emails = array();
+        if($feed){
+            foreach($feed as $entry) {
                 $ExtensionElements = $entry->getExtensionElements();
                 $ExtensionElements = $ExtensionElements[0];
                 $ExtensionAttributes = $ExtensionElements->getExtensionAttributes();
                 $emails[] .= $ExtensionAttributes['address']['value'];
-        } 
+            }
+         } 
         return  $emails;
-      }
+    }
   
   } 
