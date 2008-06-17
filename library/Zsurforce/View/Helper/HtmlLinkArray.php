7cea2787476a7861b87a7f510b9e15000943f31a
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-17 05:35:08 +0000

    - Se agregan comentarios PHPdoc - Se agrega un parámetro

--- library/Zsurforce/View/Helper/HtmlLinkArray.php
+++ library/Zsurforce/View/Helper/HtmlLinkArray.php
@@ -1,20 +1,32 @@
 <?php
-
+/**
+ * Generates a 'link' html desde un array.
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
 class Zsurforce_View_Helper_HtmlLinkArray
 {
     /**
-     * Generates a 'link' html.
+     * Generates a 'link' html desde un array.
      *
-     * @param  string $url
-     * @param  string $text
-     * @param  string $alt
+     * @param  array $param
+     * @param  string $base     
+     * 
      * @return string The element XHTML.
      */
-    public function htmlLinkArray(array $param )
-    {
-        return
-        	'<A href="' . $param['url'] .
-        	'" ALT = "' . $param['alt'] .
-        	'">' . $param['text'] . '</A>';
+    public function htmlLinkArray(array $param, $base = null )
+    {    	   	
+    	if( count($param) > 0){
+    		$ret = '<A href="' . $base . $param['url'].
+        		'" title ="' . $param['alt'] .
+        		'">' . $param['text'] . '</A>'; 	
+    	}else{
+    		$ret = NULL;
+    	}
+        return $ret;
+        	
     }
 }
+?>
