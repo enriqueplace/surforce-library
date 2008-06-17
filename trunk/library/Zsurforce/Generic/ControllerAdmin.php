44db9ee8236ef33d120fb80fbb50d4385e5b377c
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-17 04:30:23 +0000

    Se agregan comentarios

--- trunk/library/Zsurforce/Generic/ControllerAdmin.php
+++ trunk/library/Zsurforce/Generic/ControllerAdmin.php
@@ -1,15 +1,29 @@
 <?php
-
+/**
+ * Controller Genérico para definir un comportamiento común a
+ * todos los controllers para Administración de una aplicación.
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
 abstract class Zsurforce_Generic_ControllerAdmin extends Zsurforce_Generic_Controller
 {
+	/**
+	 * Verifica siempre que el usuario esté logueado, de lo contrario
+	 * interrumpe la ejecución de un controller.
+	 * 
+	 * Todo controller de un admin debería extender de este controller 
+	 * genérico
+	 */
 	final function preDispatch()
 	{
 		$auth = Zend_Auth::getInstance ();
 		if ($auth->hasIdentity ()) {
 			$this->view->usuarioLogueado = true;
 		}else {
-			die ( 'Acceso Restringido' );
+			die('Acceso Restringido');
 		}
-	}
+	}	
 }
 ?>
