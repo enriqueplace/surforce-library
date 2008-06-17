1e8215b9b1817f283b10c91e7660b2a5c9760a77
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-17 05:02:50 +0000

    Se agregan comentarios PHPdoc

--- trunk/library/Zsurforce/View/Helper/FormNumeracion.php
+++ trunk/library/Zsurforce/View/Helper/FormNumeracion.php
@@ -1,13 +1,22 @@
 <?php
-
+/**
+ * Cantidad de registros encontrados en una búsqueda
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
 class Zsurforce_View_Helper_FormNumeracion
 {
     /**
-    * cantTotal	 = cantidad de registros que devuelve la consulta
-    * cantMaxima = la cantidad maxima de registros a mostrar por pagina
-    * pagActual = pagina actual dentro del listado
-     */
-    function formNumeracion( $cantTotal=0, $cantMaxima=10, $pagActual=0 ){
+    * Cantidad de registros encontrados en una búsqueda
+    * 
+    * @param integer cantTotal 		cantidad de registros que devuelve la consulta
+    * @param integer cantMaxima 	la cantidad maxima de registros a mostrar por pagina
+    * @param integer pagActual		pagina actual dentro del listado
+    * @return xhtml
+    */
+    function formNumeracion( $cantTotal = 0, $cantMaxima = 10, $pagActual = 0 ){
         $cantInicio = ( $pagActual * $cantMaxima ) + 1;
         $cantAux = ( $pagActual + 1 ) * $cantMaxima;
         if ( $cantAux > $cantTotal){
@@ -16,7 +25,6 @@ class Zsurforce_View_Helper_FormNumeracion
             $cantFinal = $cantAux;
         }
         $xhtml = "|$cantInicio al $cantFinal de $cantTotal encontradas|";
-
         return $xhtml;
     }
 }
