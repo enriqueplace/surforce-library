69bd7dd6599db9e254358f8a36f3133789a9812a
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-17 04:49:19 +0000

    Se agregan comentarios PHPdoc

--- library/Zsurforce/Utils/Mailer.php
+++ library/Zsurforce/Utils/Mailer.php
@@ -1,22 +1,43 @@
 <?php
+/**
+ * Clase para envío de correo
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
 class Zsurforce_Utils_Mailer
 {
-    static function enviarCorreo( $to, $subject, $message, $from="admin@localhost", $conCopia="", $conCopiaOculta="")
+	/**
+	 * Método enviador, intenta enviar directamente o retorna una excepción
+	 * 
+     * @param  string $to				destinatario del correo
+     * @param  string $subject			asunto del correo
+     * @param  string $from				quién lo envía
+     * @param  string $conCopia			si se copia el envío
+     * @param  string $conCopiaOculta	si se copia el envío de forma oculta
+     * @throws Exception
+    */	
+    public static function enviarCorreo( $to, $subject, $message, $from="admin@localhost", $conCopia="", $conCopiaOculta="")
     {
-        $to .= "\r\n";
+        $to 	.= "\r\n";
+        
         $subject .= "\r\n";
+        
         $headers = "MIME-Version: 1.0\r\n";
         $headers .= "Content-type: text/html; charset=utf-8\r\n";
         $headers .= "To: $to\r\n";
         $headers .= "From: $from\r\n";
 
-        if ($conCopia!="") $headers .= "Cc: $conCopia\r\n";
-
-        if($conCopiaOculta!="") $headers .= "Bcc: $conCopiaOculta\r\n";
-
+        if ($conCopia!=""){
+        	$headers .= "Cc: $conCopia\r\n";
+        }
+        if($conCopiaOculta!=""){
+        	$headers .= "Bcc: $conCopiaOculta\r\n";
+        }
         $message = stripslashes( $message );
 
-        if (!mail ( $to, $subject, $message, $headers )) {
+        if (!mail( $to, $subject, $message, $headers )) {
             throw new Exception("No se pudo enviar el correo");
         }
     }
