f54f904d4446f35add3bc23872097f941effec0d
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-18 06:22:58 +0000

    Nuevo helper, retorna xhtml de un botón submit de un form (paramétrico)

---
+++ trunk/library/Zsurforce/View/Helper/FormularioSubmit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Retorna un botón submit de un formulario html
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
+class Zsurforce_View_Helper_FormularioSubmit
+{
+    /**
+	 * Retorna un botón submit de un formulario html
+	 * 
+     * @param  array $params		parámetros del campo (texto, valor, etc)
+     * @return xhtml 
+    */	 
+	public function formularioSubmit( $params )
+	{
+		$xhtml  = '<div id="boton_formulario">';
+		$xhtml .= ' <input type="hidden" name="id" value="'.$params['value'].'"/>'; 
+		$xhtml .= ' <input type="submit" name="add" value="'.$params['submit'].'" />';
+		$xhtml .= '</div>'; 
+
+		return $xhtml;
+	}
+}
