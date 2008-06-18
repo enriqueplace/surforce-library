6288b4970b9a3eaadacf032bd6524f2d33816105
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-18 06:31:55 +0000

    nuevo helper (no tiene mucha utilidad al momento) retorna un h1, h2, etc

---
+++ trunk/library/Zsurforce/View/Helper/PaginaTitulo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Retorna un título <h2> 
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
+class Zsurforce_View_Helper_PaginaTitulo
+{
+    /**
+     * Retorna un título h1, h2, etc
+     * @param string $text	texto del título
+     * @return xhtml
+     */
+	public function paginaTitulo( $text )
+	{
+		return "<h2>" . $text . "</h2>";
+	}
+}
