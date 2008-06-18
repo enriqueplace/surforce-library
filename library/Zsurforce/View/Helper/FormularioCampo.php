4be635bac137c29d8004c5806b3458cb1ef38b47
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2008-06-18 06:20:53 +0000

    Nuevo helper, retorna xhtml de un campo de un form (paramétrico)

---
+++ library/Zsurforce/View/Helper/FormularioCampo.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Retorna un campo de un formulario html
+ * 
+ * @category SURFORCE
+ * @package SURFORCE-LIBRARY 
+ * @license GPL v2
+ */
+class Zsurforce_View_Helper_FormularioCampo
+{
+	/**
+	 * Retorna un campo de un formulario html
+	 * 
+     * @param  array $params		parámetros del campo (texto, valor, etc)
+     * @return xhtml 
+    */	 
+	public function formularioCampo( $params )
+	{
+		$xhtml  = '<p>';
+		$xhtml .= ' <label for="'.$params['label'].'">'.$params['text'].':&nbsp;</label>'; 
+		$xhtml .= ' <input type="text" name="'.$params['label'].'" size="'.$params['size'].'" value="'.$params['value'].'"/>';
+		$xhtml .= '</p>'; 
+		return $xhtml;
+	}
+}
