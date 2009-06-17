793f9a20c0c4acdc0519d7c97d2e78d11210ea71
enriqueplace <enriqueplace@8946db71-1644-0410-beab-d1ee511207b3>
2009-06-17 03:10:59 +0000

    Agrega validación: si la fecha por parámetros está vacía, no hace nada

--- trunk/library/Zsurforce/View/Helper/DateFormat.php
+++ trunk/library/Zsurforce/View/Helper/DateFormat.php
@@ -16,10 +16,12 @@ class Zsurforce_View_Helper_DateFormat
      */
     public function dateFormat ($date)
     {
-        $fecha = explode(" ", $date);
-        // $hora = $fecha[1];
-        $f = explode("-", $fecha[0]);
-        $xhtml = $f['2'] . "/" . $f['1'] . "/" . $f['0'];
-        return $xhtml;
+        if($date != ''){
+            $fecha = explode(" ", $date);
+            // $hora = $fecha[1];
+            $f = explode("-", $fecha[0]);
+            $xhtml = $f['2'] . "/" . $f['1'] . "/" . $f['0'];
+            return $xhtml;    
+        }        
     }
 }
